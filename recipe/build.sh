2bb401ccdd165e21636369922721c5e0a4b491c4
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-11-09 12:34:21 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 tar Jxvf openkim-models-*.txz
 cd openkim-models-*
-cmake . -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/simulator-models
+CC="${CC}" FC="${FC}" CXX="${CXX}" cmake . -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/simulator-models
 make install
 cp LICENSE ..
