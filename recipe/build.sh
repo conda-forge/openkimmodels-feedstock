0c42c156077196f8441aaf344ff0d9ffc67042eb
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-02-08 22:05:50 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 tar Jxvf openkim-models-*.txz
-ls 
 cd openkim-models-*
-patch -u -p1 -i ../cmake.patch
+patch -u -p1 -i ${RECIPE_DIR}//cmake.patch
 if [[ "$(uname)" == "Darwin" ]]; then
     cmake . -DCMAKE_Fortran_COMPILER=${FC} -DCMAKE_C_COMPILER=${BUILD_PREFIX}/bin/${CC} -DCMAKE_CXX_COMPILER=${BUILD_PREFIX}/bin/${CXX} -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/simulator-models
 else
